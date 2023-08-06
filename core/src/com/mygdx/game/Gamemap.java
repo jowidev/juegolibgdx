ce2f1ba74b925d8dc51bb089a869aa40adc254b1
Jowi <xoanmanuelaltaleffpais@gmail.com>
2023-08-06 18:12:26 -0300

    viewport nuevo

--- core/src/com/mygdx/game/Gamemap.java
+++ core/src/com/mygdx/game/Gamemap.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,55 +14,50 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class Gamemap  extends ApplicationAdapter  {
-	private OrthographicCamera camera;
 	private TiledMap map;
 	private OrthogonalTiledMapRenderer mapRenderer;
 	private final Vector2 mousepos = new Vector2();
 	private FitViewport viewport;
 	private SpriteBatch batch;
-
-	float w, h; //width and height parameters
+	private Camera camera;
 
 	@Override
 	public void create() {
-		w = Gdx.graphics.getWidth();
-		h = Gdx.graphics.getHeight();
 		batch = new SpriteBatch();
-
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, (w/h) * 320, 384);
-		camera.update();
 
 		// Load the tilemap from the .tmx file
 		TiledMap map = new TmxMapLoader().load("tilemap.tmx");
 
 		// Create the map renderer
-		mapRenderer = new OrthogonalTiledMapRenderer(map);
+		mapRenderer = new OrthogonalTiledMapRenderer(map, constants.pixeltotile, batch);
 
-		viewport = new FitViewport(800, 800);
+		viewport = new FitViewport(12, 12);
+		camera = viewport.getCamera();
+		camera.position.set(constants.GAME_WORLD_WIDTH_tile/2, constants.GAME_WORLD_HEIGHT_tile/2, 0);
+
 	}
-	private void handleInput(float w) {
+	private void handleInput() {
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT) ) {
-			camera.translate(-5, 0, 0);
+			camera.translate(-.5f, 0, 0);
 			if (camera.position.x<=camera.viewportWidth / 2) {
-				camera.translate(5, 0, 0);
+				camera.translate(.5f, 0, 0);
 			}
 		} else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {		//NO PUEDE SER LO HICE SIN CHATGPT OJO ROCKSTAR
-			camera.translate(5, 0, 0);
-			if (camera.position.x>=w - camera.viewportWidth / 2) {
-				camera.translate(-5, 0, 0);
+			camera.translate(.5f, 0, 0);
+			if (camera.position.x>=constants.GAME_WORLD_WIDTH_tile - camera.viewportWidth / 2) {
+				camera.translate(-.5f, 0, 0);
 			}
 		}
 	}
 
 	public void update() {
 
-		handleInput(w);
+		handleInput();
 		camera.update();
 	}
 
 	public void resize(int w, int h) {
-		viewport.update(w, h, true);
+		viewport.update(w, h);
 	}
 
 	@Override
@@ -69,14 +65,15 @@ public class Gamemap  extends ApplicationAdapter  {
 		// Clear the screen
 		Gdx.gl.glClearColor(.8f, .8f, .8f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+		viewport.apply();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 
 		batch.end();
+
 		update();
 		camera.update();
-		mapRenderer.setView(camera);
+		mapRenderer.setView(camera.combined, 0,0 ,21, 12);
 		mapRenderer.render();
 	}
 
