c1dc226923ecc4e87d57ebb9cf83ec764bde49e1
Jowi <xoanmanuelaltaleffpais@gmail.com>
2023-08-07 16:02:16 -0300

    merge dummy branch

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -3,9 +3,16 @@ package com.mygdx.game;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.Gamemap;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		Settings settings = new Settings();
+        settings.maxWidth = 4096;
+        settings.maxHeight = 4096;
+        settings.alias = false;
+     //   TexturePacker.process(settings, "../assets", "../assets", "game");
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		//config.setWindowedMode(1366, 768);
 		config.setForegroundFPS(60);
