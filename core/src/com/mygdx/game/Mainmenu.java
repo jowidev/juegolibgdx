14e7baacbe6430f8135c01d21df357ec93db6c6e
jowidev <xoanmanuelaltaleffpais@gmail.com>
2023-09-15 19:48:56 -0300

    menu inicio

--- core/src/com/mygdx/game/Mainmenu.java
+++ core/src/com/mygdx/game/Mainmenu.java
@@ -1,23 +1,57 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.troops.game.Assets;
+import jdk.tools.jmod.Main;
 
-public class Mainmenu implements Screen {
+public class Mainmenu extends ScreenAdapter { //implements screen
+    private Stage stage;
+    private Viewport viewport;
+    private AssetManager assetManager;
+    private Skin skin;
+    private Table MainTable;
 
+    public Mainmenu(AssetManager assetManager) {
+        this.assetManager= assetManager;
+        skin = assetManager.get(Assets.SKIN);
+    }
     @Override
     public void show() {
+        viewport = new ExtendViewport(800, 600);
+        stage = new Stage(viewport);
+        MainTable = new Table();
+        stage.addActor(MainTable);
+        MainTable.setFillParent(true);
+    }
 
+    private TextButton addButton(String name){
+        TextButton button = new TextButton(name, skin);
+        MainTable.add(button);
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(.1f,.1f,.1f,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        stage.act(); //el act registra clicks, mov mouse, boludeces
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width,height);
     }
 
     @Override
--- core/src/com/troops/game/Assets.java
+++ core/src/com/troops/game/Assets.java
@@ -1,11 +1,14 @@
 package com.troops.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.SkinLoader;
 import com.badlogic.gdx.assets.loaders.TextureLoader;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.Array;
 
 
@@ -14,7 +17,7 @@ public class Assets {
 	public final Array<AtlasRegion> slimewalk;
 	public final Array<AtlasRegion> boulderwalk;
 	private final TextureAtlas atlas;
-
+	public static final AssetDescriptor<Skin> SKIN = new AssetDescriptor<Skin>("skin/freezing-ui.json", Skin.class, new SkinLoader.SkinParameter("skin/freezing-ui.atlas"))
 	public Assets() {
 		manager.load("slimes/slime-idle-0.png", TextureAtlas.class);
 		TextureLoader.TextureParameter param = new TextureLoader.TextureParameter();
@@ -22,5 +25,8 @@ public class Assets {
 		this.atlas = new TextureAtlas(Gdx.files.internal("game.atlas"));
         this.slimewalk = atlas.findRegions("slime");
         this.boulderwalk = atlas.findRegions("boulder");
+	}
+	public void loadAll() {
+		manager.load(SKIN);
 	}
 }
